0a528a27f30e457be65ded131af3bc1bcf23ff08
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-06-12 01:02:31 +0200

    New Spellcast: in progress!

--- test/spellcast-test.js
+++ test/spellcast-test.js
@@ -1,9 +1,9 @@
 /*
-	The Cedric's Swiss Knife (CSK) - CSK Spellcast test suite
+	Spellcast
+	
+	Copyright (c) 2014 - 2016 Cédric Ronvel
 	
 	The MIT License (MIT)
-	
-	Copyright (c) 2014 Cédric Ronvel 
 	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -12,16 +12,16 @@
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
 	
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 	
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 */
 
 /* jshint unused:false */
