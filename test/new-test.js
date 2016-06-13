615e1683dc8daefbe5b25e990a58fb6af3049fe8
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-06-13 14:02:45 +0200

    wip

--- test/new-test.js
+++ test/new-test.js
@@ -50,7 +50,7 @@ describe( "..." , function() {
 		var book = spellcast.load( __dirname + '/spellbook.kfg' ) ;
 		//deb( book ) ;
 		
-		book.script.get( 'ls' ).run( book , null , done ) ;
+		book.cast( 'ls' , done ) ;
 	} ) ;
 } ) ;
 
