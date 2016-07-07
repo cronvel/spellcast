78e4a455d22084c9d080eadfbb87cddd72a63b5a
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-07-07 15:34:42 +0200

    new tags: append, concat, glob

--- sample/firestaff.js
+++ sample/firestaff.js
@@ -1,11 +1,11 @@
 
-exports.fireball = function( book , tag , context )
+exports.fireball = function( book , tag , execContext )
 {
 	console.log( "ZASH... ROOOOARRRR-CRASHHHHH!" ) ;
 	console.log( tag.proxy.data.wizard + ' killed the ' + tag.content + "..." ) ;
 } ;
 
-exports['delayed fireball'] = function( book , tag , context , callback )
+exports['delayed fireball'] = function( book , tag , execContext , callback )
 {
 	console.log( "ssssshhhhh... SSSSSHHHHH..." ) ;
 	setTimeout( function() {
