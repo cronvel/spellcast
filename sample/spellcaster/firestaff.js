ebe6cd35973fbbd34082a32a74aee0d60448efbf
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-08-16 16:37:19 +0200

    refacto: initial dev ok!

--- sample/spellcaster/firestaff.js
+++ sample/spellcaster/firestaff.js
@@ -1,16 +1,16 @@
 
-exports.fireball = function( book , tag , execContext )
+exports.fireball = function( book , tag , ctx )
 {
 	console.log( "ZASH... ROOOOARRRR-CRASHHHHH!" ) ;
-	console.log( tag.proxy.data.wizard + ' killed the ' + tag.content + "..." ) ;
+	console.log( ctx.data.wizard + ' killed the ' + tag.getFinalContent( ctx.data ) + "..." ) ;
 } ;
 
-exports['delayed fireball'] = function( book , tag , execContext , callback )
+exports['delayed fireball'] = function( book , tag , ctx , callback )
 {
 	console.log( "ssssshhhhh... SSSSSHHHHH..." ) ;
 	setTimeout( function() {
 		console.log( "ROOOOARRRR-CRASHHHHH!" ) ;
-		console.log( tag.proxy.data.wizard + ' killed the ' + tag.content + "..." ) ;
+		console.log( ctx.data.wizard + ' killed the ' + tag.getFinalContent( ctx.data ) + ", with a delay..." ) ;
 		callback() ;
 	} , 500 ) ;
 } ;
