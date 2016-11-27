d1321bb2d4a041d899689bd7b63119908febef52
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-11-27 21:49:33 +0100

    Electron menu tests

--- electron/app.js
+++ electron/app.js
@@ -9,6 +9,7 @@ const app = electron.app ;
 
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow ;
+const Menu = electron.Menu ;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -20,8 +21,32 @@ var args = require( 'minimist' )( process.argv.slice( 2 ) ) ;
 
 
 
-function createWindow ()
+function onReady()
 {
+	var template = [
+		{
+			label: 'Join',
+			submenu: [
+				{
+					label: 'Connect' ,
+					click: function() { console.log( 'Connect' ) ; }
+				} ,
+			]
+		} ,
+		{
+			label: 'Server',
+			submenu: [
+				{
+					label: 'Create' ,
+					click: function() { console.log( 'Create' ) ; }
+				} ,
+			] ,
+		} ,
+	] ;
+	
+	var menu = Menu.buildFromTemplate( template ) ;
+	Menu.setApplicationMenu( menu ) ;
+	
 	var url ;
 	
 	if ( args.url )
@@ -44,7 +69,6 @@ function createWindow ()
 	// Create the browser window.
 	mainWindow = new BrowserWindow( { width: 800 , height: 600 } ) ;
 	
-	mainWindow.setMenu( null ) ;
 	
 	// and load the index.html of the app.
 	mainWindow.loadURL( url ) ;
@@ -66,7 +90,7 @@ function createWindow ()
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on( 'ready' , createWindow ) ;
+app.on( 'ready' , onReady ) ;
 
 
 
